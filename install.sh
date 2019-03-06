3bbb0419eab2ad52307305a4770f007a43afacd3
Jake <jake@cookietronics.net>
2019-03-05 19:12:30 -0500

    Adding install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INSTALLDIR=$HOME/irssi-emojis
+IRSSIDIR=$HOME/.irssi
+
+echo -e "✨ Installing imojis for irssi! ✨ "
+echo -e ""
+ln -sf $INSTALLDIR/emojis-db.dat $IRSSIDIR/emojis-db.dat
+ln -sf $INSTALLDIR/emojis.pl $IRSSIDIR/scripts/emojis.pl
+
+if [ -d $IRSSIDIR/scripts/autorun ]; then
+  echo -e "➕Enabling autorun of the script."
+  ln -sf $INSTALLDIR/emojis.pl $IRSSIDIR/scripts/autorun/emojis.pl
+fi
+echo -e ""
+echo -e "ℹ️Load load script in irssi using '/script load emojis'"
+echo -e ""
+echo -e "Thanks! 👍"
